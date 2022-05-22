0a3436023a549893d96da84fba33074cb8190909
filipvlad2 <filip.vlad2@yahoo.com>
2022-05-22 17:51:48 +0300

    Update Dictionary.js

--- Dictionary.js
+++ Dictionary.js
@@ -1,12 +1,12 @@
 let dictionary = new Array();
 
-function adaugareCuvant() {
+function wordAdd() {
 	let currentSave = document.getElementById('saveWord').value;
 	dictionary.push(currentSave); 
 	return false;
 }
 
-function cautareCuvant() {
+function wordSearch() {
 	let currentSearch = document.getElementById('searchWord').value;
 	if (dictionary.includes(currentSearch)) {
 		alert("Word found!");
